c4bdb334f65a14b4d1dd98c9ae12e79e8d8bdab1
wangjiacheng111 <JiaCheng1995>
2016-10-29 15:27:12 +0800

    选项卡2.0JS

--- 选项卡/选项卡2.0.js
+++ 选项卡/选项卡2.0.js
@@ -1,6 +1,5 @@
 var oMenu = document.getElementById('first');
 var ali = oMenu.getElementsByTagName('li');
-var otext = document.getElementById('text');
 var adiv = oMenu.getElementsByTagName('div');
 for(var i=0;i<ali.length;i++){
     ali[i].index = i;
