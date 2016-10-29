ef853877e748e99e35e743ddd57bf0cbfb68b6d5
wangjiacheng111 <JiaCheng1995>
2016-10-29 13:09:40 +0800

    多级导航菜单总览

--- 多级导航菜单/多级导航菜单.js
+++ 多级导航菜单/多级导航菜单.js
@@ -1,12 +1,12 @@
 var Menu = document.getElementById('menu');
 var MainMenu = Menu.getElementsByTagName('li');
 for(var i=0; i<MainMenu.length; i++){
-    if(MainMenu[i].className == 'main_menu') {
-        MainMenu[i].onmouseover = function () {
+    if(MainMenu[i].className == 'main_menu'|| MainMenu[i].className == 'main_menu magin-menu'){
+        MainMenu[i].onmouseover = function (){
             var submenu = this.getElementsByTagName('ul')[0];
             submenu.style.display = 'block';
         };
-        MainMenu[i].onmouseout = function () {
+        MainMenu[i].onmouseout = function (){
             var submenu = this.getElementsByTagName('ul')[0];
             submenu.style.display = 'none';
         };
@@ -22,4 +22,5 @@ for(var i=0; i<MainMenu.length; i++){
 //            var oUl = this.getElementsByTagName("ul")[0];
 //            oUl.style.display = "none";
 //        };
-//    } //IE8以下不支持 但是此方式可以省略判断
+//    } //IE8
+
